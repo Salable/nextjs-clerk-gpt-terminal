78a06caedd50e6459c51db57b046f719789e5c3c
nealriley <neal.riley@gmail.com>
2023-04-23 13:56:06 +0000

    switch to yarn and start adding comments

--- pages/api/queryGPT.js
+++ pages/api/queryGPT.js
@@ -5,8 +5,11 @@ import { SerpAPI } from "langchain/tools";
 import { Calculator } from "langchain/tools/calculator";
 const { SalableApi } = require("@salable/node-sdk");
 
+// callGPT is a function that takes a prompt as input and returns an output from langchain
 const callGPT = async function (input) {
+  // Default to 0 temp OpenAI model
   const model = new OpenAI({ temperature: 0 });
+  // Add SerpAPI and Calculator tools to our chain of capabilities
   const tools = [
     new SerpAPI(process.env.SERPAPI_API_KEY, {
       location: "Austin,Texas,United States",
@@ -15,17 +18,19 @@ const callGPT = async function (input) {
     }),
     new Calculator(),
   ];  
-
+  // Initialize the agent executor with the options agentType as zero-shot
   const executor = await initializeAgentExecutorWithOptions(tools, model, {
     agentType: "zero-shot-react-description",
   });
-  console.log("Loaded agent.");
 
+  // Execute the agent with the input
   console.log(`Executing with input "${input}"...`);
   const result = await executor.call({ input });
   return result
 }
 
+// handler is the function that is called when the api is called
+// The handler checks for a valid session, whether the user is licensed with Salable, and then calls callGPT
 export default async function handler(req, res) {
   const { sessionId, userId } = getAuth(req);
 
--- pages/ssr-demo/index.js
+++ pages/ssr-demo/index.js
@@ -3,44 +3,21 @@ import { clerkClient, getAuth, buildClerkProps } from "@clerk/nextjs/server";
 import { useUser } from "@clerk/nextjs";
 import React from "react";
 
-
-async function getData(userId, url) {
-  console.log("Getting data for user:", userId)
-  const response = await fetch('https://winter-night-9989.fly.dev/?url='+url);
-  const data = await response.json()
-  return data;
-}
-async function mockGetPosts(userId, url) {
-  const data = await getData(userId,url);
-  console.log("Data from mockGetPosts: ", data)
-  return data
-};
-
 export const getServerSideProps = async ({ req, query }) => {
   const { userId } = getAuth(req);
-  const url = query["url"];
   const user = userId ? await clerkClient.users.getUser(userId) : null;
-
-  const scraperResponse = await mockGetPosts(user.id, url);
   return { props: {  user: user, scraperResponse: scraperResponse} };
 };
 
 function SSRDemoPage ({scraperResponse}) {
 
   const { isSignedIn, isLoaded } = useUser();
-  // Code hightlighting
-  React.useEffect(() => {
-    if (window.Prism) {
-      window.Prism.highlightAll();
-    }
-  });
 
   return (
     <div className={styles.container}>
       <main className={styles.main}>
         <h1 className={styles.title}>Data on</h1>
         <p className={styles.description}>
-          {scraperResponse.metadata.text}
         </p>
       </main>
     </div>
